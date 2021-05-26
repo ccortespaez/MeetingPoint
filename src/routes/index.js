62d414785d447c187d31837a27ffbd862dd2620e
Cristobal Cortés <cristobal.cortespaez@gmail.com>
2021-05-26 17:45:41 -0400

    Pagina de  detalle de cada evento

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,15 @@ const pool = require('../database');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', isLoggedIn, async (req, res) => {
+router.get('/', async (req, res) => {
     const data = await pool.query('SELECT * FROM eventos');
     res.render('index', {data});
+});
+
+router.get('/view-event/:id', async (req, res) => {
+    const {id} = req.params;
+    const data = await pool.query('SELECT * FROM eventos WHERE id = ?', [id]);
+    res.render('event', {data: data[0]});
 })
 
 module.exports = router;
