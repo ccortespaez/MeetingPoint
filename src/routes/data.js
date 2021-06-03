a548a342a31338bb4cd69722fa99a97372635904
Cristobal Cortés <cristobal.cortespaez@gmail.com>
2021-06-03 17:23:47 -0400

    Se añadio nuevo rol, usuario organizador puede crear eventos

--- src/routes/data.js
+++ src/routes/data.js
@@ -4,29 +4,28 @@ const {isLoggedIn} = require('../lib/auth');
 const pool = require('../database');
 
 router.get('/admin', isLoggedIn, async(req, res) => {
-    const user = await pool.query('SELECT * FROM usuarios WHERE id = ?', [req.user.id])
+    const user = await pool.query('SELECT * FROM usuarios WHERE id = ?', [req.user.id]);
     const data = await pool.query('SELECT * FROM eventos', [req.user.id]);
-    if(req.user.rol == "admin"){
+    if(req.user.rol_id == 1){
         res.render("admin/admin", {
             name: req.user.username , user, data
         });
-    }else if(req.user.rol == "user"){
+    }else if(req.user.rol_id == 2 || req.user.rol_id == 3){
         res.status(404).send("Acceso denegado");
-    }
-    
-})
+    } 
+});
 
 router.get('/admin/add', isLoggedIn, async(req, res) => {
-    if(req.user.rol == "admin"){
+    if(req.user.rol_id == 1){
         res.render("admin/add", {
             user: req.user.fullname
         });
-    }else if(req.user.rol == "user"){
+    }else if(req.user.rol_id == 2 || req.user.rol_id == 3){
         res.status(404).send("Acceso denegado");
     } 
-})
+});
 
-router.post('/admin/add-data', async(req, res) => {
+router.post('/admin/add-data', isLoggedIn, async(req, res) => {
    const { title, description, image, phone, animator, artist, datestart, datefinish, hour, location, type, age, streaming, producer, capacity } = req.body;
    const newData = {
        title, 
@@ -44,12 +43,12 @@ router.post('/admin/add-data', async(req, res) => {
        streaming, 
        producer, 
        capacity,
-       usuario_id: req.user.id
+       usuarios_id: req.user.id
    }
    await pool.query('INSERT INTO eventos SET ?', [newData]);
    req.flash('success', 'New data added');
    res.redirect('/admin');
-})  
+});  
 
 router.get('/admin/data-delete/:id', isLoggedIn, async (req, res) => {
     const {id} = req.params;
@@ -68,7 +67,7 @@ router.post('/admin/data-edit/:id', async (req, res) => {
     const {id} = req.params;
     const { title, description, image, phone, animator, artist, datestart, datefinish, hour, location, type, age, streaming, producer, capacity } = req.body;
     const newData = {
-        title, 
+       title, 
        description,
        image,
        phone,
@@ -87,6 +86,6 @@ router.post('/admin/data-edit/:id', async (req, res) => {
     await pool.query('UPDATE eventos SET ? WHERE id = ?', [newData, id]);
     req.flash('success', 'Data updated');
     res.redirect('/admin');
-})
+});
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const pool = require('../database');
 const express = require('express');
 const router = express.Router();
+const {isLoggedIn} = require('../lib/auth');
 
 
 router.get('/', async (req, res) => {
@@ -25,8 +26,39 @@ router.get('/events', async (req, res) => {
     res.render('events', {data})
 });
 
-router.get('/my-event', (req, res) => {
-    res.render('myevent');
+router.get('/my-event', isLoggedIn, async (req, res) => {
+    const user = await pool.query('SELECT * FROM usuarios WHERE id = ?', [req.user.id]);
+    if(req.user.rol_id == 1 || req.user.rol_id == 2){
+        res.render('myevent');
+    }else if(req.user.rol_id == 3){
+        res.render('index');
+    } 
 })
+
+router.post('/my-event/add-data', isLoggedIn, async(req, res) => {
+    const { title, description, image, phone, animator, artist, datestart, datefinish, hour, location, type, age, streaming, producer, capacity } = req.body;
+    const newData = {
+        title, 
+        description,
+        image,
+        phone,
+        animator,
+        artist,
+        datestart, 
+        datefinish,
+        hour,
+        location,
+        type,
+        age,
+        streaming, 
+        producer, 
+        capacity,
+        usuarios_id: req.user.id
+    }
+    await pool.query('INSERT INTO eventos SET ?', [newData]);
+    req.flash('success', 'New data added');
+    res.redirect('/events');
+ });  
+
 
 module.exports = router;
